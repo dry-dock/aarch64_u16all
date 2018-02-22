e71af6eed0e2796f0d01dc6b85a344bd17543b86
Niranjan Godbole <niranjan8192@gmail.com>
2018-02-22 10:41:16 +0000

    adds postgres service

---
+++ version/postgres.sh
@@ -0,0 +1,17 @@
+
+#!/bin/bash -e
+
+POSTGRES_VERSION=9.5
+echo "================= Installing Postgres $POSTGRES_VERSION ==================="
+apt-get install -y postgresql-"$POSTGRES_VERSION" postgresql-server-dev-"$POSTGRES_VERSION" postgis
+
+echo "================= Adding PostgreSQL cnf ==================="
+cp -rf /u16all/pg_hba.cnf /etc/postgresql/"$POSTGRES_VERSION"/main/pg_hba.conf
+
+mkdir /etc/ssl/private-copy
+mv /etc/ssl/private/* /etc/ssl/private-copy/
+rm -r /etc/ssl/private
+mv /etc/ssl/private-copy /etc/ssl/private
+chmod -R 0700 /etc/ssl/private
+chown -R postgres /etc/ssl/private
+
