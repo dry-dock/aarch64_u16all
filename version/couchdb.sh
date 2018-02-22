3939fb6646d340b501ba63ece9f26777a8a885b5
Niranjan Godbole <niranjan8192@gmail.com>
2018-02-22 07:17:53 +0000

    make couchdb executable

--- version/couchdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-VERSION=1.6.0
-echo "================= Installing CouchDB $VERSION ==================="
-sudo apt-get install couchdb="$VERSION"* couchdb-bin="$VERSION"* couchdb-common="$VERSION"* -yf
-
