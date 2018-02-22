0f790c8ee238ef8cb370b88dc9ad3f79a6cc80a2
Niranjan Godbole <niranjan8192@gmail.com>
2018-02-22 09:09:48 +0000

    fix memcached version

--- version/memcached.sh
+++ version/memcached.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-MEM_VERSION=1.5.4
+MEM_VERSION=1.4.25
 LIB_VERSION=1.0.18
 
 echo "================= Installing MemCached Prereqs ==================="
 apt-get install -y libevent-dev libsasl2-dev
 
 echo "================= Installing Memcached and LibMemCached ==================="
-apt-get install memcached="$MEM_VESRION"* libmemcached11="$LIB_VERSION"*
+apt-get install -y memcached="$MEM_VERSION"* libmemcached11="$LIB_VERSION"*
 
