6eae072e2cce2d7895de327435a694bc8ddb4b39
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-28 18:11:40 +0530

    Update sqlite.sh

--- version/sqlite.sh
+++ version/sqlite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-SQLITE_VERSION=3.11.0
+SQLITE_VERSION=3.22.0
 echo "================= Installing SQLite $SQLITE_VERSION ==================="
 
 apt-get install sqlite3="$SQLITE_VERSION"*
