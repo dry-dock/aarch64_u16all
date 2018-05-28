a9e604ec3481d235a11e5b1ce327527e19f11c84
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-05-28 18:56:35 +0530

    Update couchdb.sh

--- version/couchdb.sh
+++ version/couchdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-VERSION=2.1.1
+VERSION=1.6.0
 echo "================= Installing CouchDB $VERSION ==================="
 sudo apt-get install couchdb="$VERSION"* couchdb-bin="$VERSION"* couchdb-common="$VERSION"* -yf
 
