f7f66e95c10690a300bbb134c083a0022f78b3c3
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-24 14:27:49 +0530

    Update postgres.sh

--- version/postgres.sh
+++ version/postgres.sh
@@ -1,22 +1,16 @@
+
 #!/bin/bash -e
 
-sudo apt-get install -y wget ca-certificates
-
-POSTGRES_VERSION=10
+POSTGRES_VERSION=9.5
 echo "================= Installing Postgres $POSTGRES_VERSION ==================="
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y postgresql-"$POSTGRES_VERSION" postgresql-server-dev-"$POSTGRES_VERSION"
-sudo apt-get install postgis
+apt-get install -y postgresql-"$POSTGRES_VERSION" postgresql-server-dev-"$POSTGRES_VERSION" postgis
 
-# Fix bug https://github.com/docker/docker/issues/783
-# which prevents postgresql from staring when using aufs
-# This workaround proposed in the comment to the issue: https://github.com/docker/docker/issues/783#issuecomment-56013588
-# I've test this on my own builds
+echo "================= Adding PostgreSQL cnf ==================="
+cp -rf /u16all/pg_hba.cnf /etc/postgresql/"$POSTGRES_VERSION"/main/pg_hba.conf
+
 mkdir /etc/ssl/private-copy
 mv /etc/ssl/private/* /etc/ssl/private-copy/
 rm -r /etc/ssl/private
 mv /etc/ssl/private-copy /etc/ssl/private
 chmod -R 0700 /etc/ssl/private
-chown -R postgres /etc/ssl/privat
+chown -R postgres /etc/ssl/private
