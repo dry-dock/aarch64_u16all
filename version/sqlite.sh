15f08f97cc982b1d32bf53f276116cb70cb0b499
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-09-24 13:42:41 +0530

    Update sqlite.sh

--- version/sqlite.sh
+++ version/sqlite.sh
@@ -4,5 +4,6 @@ SQLITE_VERSION=3.11.0
 echo "================= Installing SQLite $SQLITE_VERSION ==================="
 
 sudo add-apt-repository ppa:jonathonf/backports
-apt-get install sqlite3="$SQLITE_VERSION"*
+sudo apt-get update && sudo apt-get install sqlite3="$SQLITE_VERSION"*
+
 
